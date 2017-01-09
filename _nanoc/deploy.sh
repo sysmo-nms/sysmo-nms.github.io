51de398e176c283532c3b39dd9aa317dd741baf1
Sébastien Serre <sserre.bx@gmail.com>
2017-01-09 23:16:30 +0100

    Website content modif

--- _nanoc/deploy.sh
+++ _nanoc/deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# function ping_site_maps {
-#   curl www.google.com/webmasters/sitemaps/ping?sitemap=http://www.sysmo.io/sitemap.xml
-#   echo ""
-#   curl www.bing.com/webmaster/ping.aspx?siteMap=http://www.sysmo.io/sitemap.xml
-# }
+function ping_site_maps {
+  curl www.google.com/webmasters/sitemaps/ping?sitemap=http://www.sysmo.io/sitemap.xml
+  echo ""
+  curl www.bing.com/webmaster/ping.aspx?siteMap=http://www.sysmo.io/sitemap.xml
+}
 
-# echo -n "Is it ok to call ping sitemaps? (y/n): "
-# read REP
+echo -n "Is it ok to call ping sitemaps? (y/n): "
+read REP
 
-# case "$REP" in
-#   "y")
-#     ping_site_maps
-#     ;;
-#   "Y")
-#     ping_site_maps
-#     ;;
-#   *)
-#     exit
-#     ;;
-# esac
+case "$REP" in
+  "y")
+    ping_site_maps
+    ;;
+  "Y")
+    ping_site_maps
+    ;;
+  *)
+    exit
+    ;;
+esac
