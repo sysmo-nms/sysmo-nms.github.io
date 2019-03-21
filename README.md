f418d88df787bb48d4e368584b67c49469d0e4eb
seb <sserre@gmail.com>
2019-03-21 09:29:10 +0100

    new url

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-www.sysmo.io
-============
+sysmo-nms.github.io
+===================
 
 See also the [wiki](https://github.com/sysmo-nms/sysmo-nms.github.io/wiki).
--- _nanoc/deploy.sh
+++ _nanoc/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function ping_site_maps {
-  curl www.google.com/webmasters/sitemaps/ping?sitemap=http://www.sysmo.io/sitemap.xml
+  curl www.google.com/webmasters/sitemaps/ping?sitemap=https://sysmo-nms.github.io/sitemap.xml
   echo ""
-  curl www.bing.com/webmaster/ping.aspx?siteMap=http://www.sysmo.io/sitemap.xml
+  curl www.bing.com/webmaster/ping.aspx?siteMap=https://sysmo-nms.github.io/sitemap.xml
 }
 
 echo -n "Is it ok to call ping sitemaps? (y/n): "
